8346a7bcecaaa37816b486f1c4e76c63d0cebf57
tannerwwhite <30422707+tannerwwhite@users.noreply.github.com>
2017-11-27 10:34:20 -0700

    html done, begin JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+var playerChar = [
+  {character: 'Luke Skywalker',img: 'assets/images/',hp:, ap:, ca:},
+  {character: 'Storm Trooper',img: 'assets/images/',hp:, ap:, ca:}
+  {character: 'Darth Vader',img: 'assets/images/',hp:, ap:, ca:}
+  {character: 'Boba Fett ',img: 'assets/images/',hp:, ap:, ca:}
+];
+
+
+
+
+}
